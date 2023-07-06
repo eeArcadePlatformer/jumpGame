02ffe2a7571ca105aab68dbd9f6c44cd6f78c9ba
spaceOfSoul <ychh1123@gmail.com>
2023-07-06 14:54:34 +0900

    walking animation

--- main.py
+++ main.py
@@ -25,18 +25,33 @@ def draw_grid():
 
 class Player():
     def __init__(self,x,y):
-        img = pg.image.load('img/guy1.png')
-        self.image = pg.transform.scale(img,(20,40))
+        # 애니메이션을 위한 이미지 목록
+        self.images_right = []
+        self.images_left = []
+        self.index = 0 # animation index
+        self.counter = 0
+        for num in range(1, 5):
+            img_right = pg.image.load(f'img/guy{num}.png')
+            img_right = pg.transform.scale(img_right, (20, 40))
+            img_left = pg.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        
+        # images_right의 idx번째 transform
+        self.image = self.images_right[self.index]
+        
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.velocuty_y = 0
         self.jumped = False
         self.speed_x = 2.5
+        self.direction = 0 # right : 1, left : -1
         
     def update(self):
         dx = 0
         dy = 0
+        walk_cooldown = 5
         
         # get key press
         key = pg.key.get_pressed()
@@ -44,8 +59,12 @@ class Player():
         # horizontal move
         if key[pg.K_LEFT]:
             dx -=self.speed_x
+            self.counter += 1
+            self.direction =-1
         if key[pg.K_RIGHT]:
             dx += self.speed_x
+            self.counter += 1
+            self.direction =1
             
         # jump
         if key[pg.K_SPACE] and self.jumped == False:
@@ -54,6 +73,26 @@ class Player():
         if key[pg.K_SPACE] == False:
             self.jumped = False
             
+        if key[pg.K_LEFT] == False and key[pg.K_RIGHT] == False:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+        
+        # handle animation
+        if self.counter > walk_cooldown:
+            self.counter = 0	
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+        
+        
         # gravity
         self.velocuty_y +=0.5
         if self.velocuty_y >5:
