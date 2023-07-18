98bc5d39533f17f9f1d0f8750aa368ca1b73dded
ParkJeongYoon <yjym797979@naver.com>
2023-07-18 17:33:38 +0900

    etc classes add

--- classes/coin.py
+++ classes/coin.py
@@ -1,5 +1,9 @@
-import pygame as pg
+import pygame
 
-class Coin():
-    def __init__(self):
-        pass
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y, tile_size):
+        super().__init__()
+        self.image = pygame.image.load('img/coin.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (tile_size // 2, tile_size // 2))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
