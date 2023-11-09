656c96f652dabaa8d0cca678365e9ba544a2583e
ParkJeongYoon <yjym797979@naver.com>
2023-11-10 00:12:39 +0900

    gameguide

--- gameguide.py
+++ gameguide.py
@@ -1,21 +1,23 @@
 import pygame as pg
+from pygame.locals import *
 import sys
+
+from main import *
+from utils import *
 
 class GameIntro:
     def __init__(self, screen):
         self.screen = screen
         self.font = pg.font.Font(None, 36)  # 사용할 폰트 설정
-        self.text_color = (255, 255, 255)  # 텍스트 색상 설정
+        self.text_color = (0, 0, 0)  # 텍스트 색상 설정
 
         self.instructions = [
-            "Welcome to My Platformer Game!",
+            "Welcome to Our Platformer Game!",
             "Instructions:",
             "- Use arrow keys to move left and right.",
             "- Press the spacebar to jump.",
             "Press Enter to start the game."
         ]
-
-        self.image = pg.image.load("intro_image.png")  # 이미지 파일 경로 설정
 
     def show_intro(self):
         intro = True
@@ -28,14 +30,11 @@ class GameIntro:
                     if event.key == pg.K_RETURN:
                         intro = False
 
-            self.screen.fill((0, 0, 0))  # 배경을 검은색으로 설정
-
             # 이미지를 화면에 표시
-            image_rect = self.image.get_rect(center=self.screen.get_rect().center)
-            self.screen.blit(self.image, image_rect)
+            screen.blit(bg_img, (0, 0))
 
             # 텍스트를 화면에 표시
-            y = 300
+            y = 400
             for line in self.instructions:
                 text_surface = self.font.render(line, True, self.text_color)
                 text_rect = text_surface.get_rect(center=(self.screen.get_width() // 2, y))
@@ -47,8 +46,8 @@ class GameIntro:
 if __name__ == "__main__":
     pg.init()
 
-    screen_width = 800
-    screen_height = 600
+    screen_width = 1000
+    screen_height = 1000
     screen = pg.display.set_mode((screen_width, screen_height))
     pg.display.set_caption('Game Intro')
 
@@ -65,5 +64,3 @@ if __name__ == "__main__":
             run = False
 
         game_intro.show_intro()
-
-    pg.quit()
